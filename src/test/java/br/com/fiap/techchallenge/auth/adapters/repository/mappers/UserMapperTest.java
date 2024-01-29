fdc0c3dca9edff38c5f0f4e9424cf87e7ef6cb10
Vanderson Nunes <vann.nunes@gmail.com>
2024-01-29 20:48:41 -0300

    Test coverage

---
+++ src/test/java/br/com/fiap/techchallenge/auth/adapters/repository/mappers/UserMapperTest.java
@@ -0,0 +1,44 @@
+package br.com.fiap.techchallenge.auth.adapters.repository.mappers;
+
+import br.com.fiap.techchallenge.auth.adapters.repository.models.User;
+import br.com.fiap.techchallenge.auth.core.domain.entities.enums.UserRole;
+import br.com.fiap.techchallenge.auth.core.dtos.UserDTO;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserMapperTest {
+
+
+    @Test
+    void testToUser() {
+        UserMapper userMapper = new UserMapper();
+        UserDTO dto = new UserDTO("username", "name", "email", List.of(UserRole.CUSTOMER));
+        User result = userMapper.toUser(dto);
+
+
+        assertThat(result).isNotNull();
+        assertThat(result.getUsername()).isEqualTo(dto.username());
+        assertThat(result.getName()).isEqualTo(dto.name());
+        assertThat(result.getEmail()).isEqualTo(dto.email());
+        assertThat(result.getRoles()).isEqualTo(dto.roles());
+    }
+
+    @Test
+    void testToUserDTO() {
+        UserMapper userMapper = new UserMapper();
+        User user = new User("username", "name", "email", List.of(UserRole.CUSTOMER));
+        UserDTO result = userMapper.toUserDTO(user);
+
+        assertThat(result).isNotNull();
+        assertThat(result.id()).isEqualTo(user.getId());
+        assertThat(result.name()).isEqualTo(user.getName());
+        assertThat(result.username()).isEqualTo(user.getUsername());
+        assertThat(result.email()).isEqualTo(user.getEmail());
+        assertThat(result.roles()).isEqualTo(user.getRoles());
+    }
+
+}
